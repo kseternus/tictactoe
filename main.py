d87b4043d9285dce658e9279d498dc3be9336315
Kamil Seternus <60841895+kseternus@users.noreply.github.com>
2020-02-12 15:50:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # Kamil Seternus 2020
 # Gra kółko i krzyżyk
-# Projekt v1.0
+# Projekt v1.1
 
 # ********** Zmienne Globalne **********
 
-# Utworzenie tablicy która jest tablicą(ang. board) gry
-board = ["-", "-", "-",
-         "-", "-", "-",
-         "-", "-", "-"]
+# Utworzenie tablicy która jest polem gry
+board = [' '] * 9
 
 # Utworzenie zmiennej definiującej czy gra ma dalej się toczyć, zmienna typu boolean
 game_still_on = True
@@ -15,10 +13,60 @@ game_still_on = True
 # Utworzenie zmiennej
 winner = None
 
+# Dodanie zmiennych śledzących liczbę wygranych dla X i Y
+x_wins = 0
+o_wins = 0
+ties = 0
+
 # Ustawienie aktualnego gracza na krzyżyk, jako, że krzyżyk zawsze rozpoczyna grę
-current_player = 'X'
+current_player = ''
+
+play_again = True
 
 # *************** Funkcje ***************
+
+# Utworzenie funkcji pozwalającej wybrać graczowi czy chce zaczynać jako X czy jako O
+def choose_player():
+    # Ustawienie zmiennej globalnej current_player
+    global current_player
+
+    # pętla działająca na zmiennej boolean, jeśli zmienna valid jest nie jest prawdziwa to pętla while poniżej
+    # sprawdza czy wpisany gracz jest zgodny
+    valid = False
+    while not valid:
+
+        # Pętla while sprawdzająca czy wpisana wartość jest X lub O, jeśli tak niezależnie od wielkości wpisanej
+        # wartości, przypisze ją do current_player i ustawi danego gracza jako startowego
+        while current_player not in ['X', 'O']:
+            current_player = input('Choose if you want to be X or O: ').upper()
+        if current_player == 'X' or current_player == 'O':
+            valid = True
+
+
+# Utworzenie funkcji odpowiedzialnej za powtórzenie rozgrywki po jej zakończeniu
+def next_game():
+
+    # Załadowanie globalnych zmiennych
+    global board, winner, play_again, game_still_on
+    # Zmienna do której przypiszemy chęć lub rezygnację z dalszej gry
+    next_game = ''
+    # Pętla sprawdzająca czy wybrana opcja jest zgodna (Y/N) jeśli nie to powtarza prośbę. Przypisuje ją do zmiennej
+    # next game
+    while next_game not in ['Y', 'N']:
+        next_game = input('Do you want to play again? (Y/N)').upper()
+
+    # Jeśli wyrazimy chęć zagrania ponownie zmienna ma wartość Y. Resetujemy tablicę board (planszę gry)
+    # zmieniamy wartość globalnej zmiennej na play_again na True oraz game_still_on na True
+    # w przeciwnym wypadku gra się końćzy
+    if next_game == 'Y':
+        board = [' '] * 9
+        play_again = True
+        game_still_on = True
+    elif next_game == 'N':
+        play_again = False
+
+    return
+
 
 # Utworzenie funkcji odpowiedzialnej za wyświetlanie tablicy z grą
 def display_board():
@@ -48,8 +96,8 @@ def handle_turn(player):
         # wartości 1 bez pomniejszenia jej skutkowało by ustawieniem kółka/krzyżyka w pozycji o 1)
         position = int(position) - 1
 
-        # Jeśli pozycja na planszy jest wolna('-') to warunek valid jest prawdziwy
-        if board[position] == '-':
+        # Jeśli pozycja na planszy jest wolna(' ') to warunek valid jest prawdziwy
+        if board[position] == ' ':
             valid = True
         else:
             print('Place occupied! Choose another position.')
@@ -66,9 +114,9 @@ def check_rows():
     global game_still_on
     # Zmienna row1 przypisana wartości z tablicy o indeksie 0 a ta pozycja tablicy jest taka sama jak pozycja 1 i 2
     # to mamy zwycięzce w rządzie pierwszym. Aby uniknąć wygranej z powodu placeholdera - musi być też różna od -
-    row1 = board[0] == board[1] == board [2] != '-'
-    row2 = board[3] == board[4] == board [5] != '-'
-    row3 = board[6] == board[7] == board [8] != '-'
+    row1 = board[0] == board[1] == board [2] != ' '
+    row2 = board[3] == board[4] == board [5] != ' '
+    row3 = board[6] == board[7] == board [8] != ' '
 
     # Jeśli w którymś rzędzie jest wygrany to ustaw globalną zmienną na False tym samym zakończ grę
     if row1 or row2 or row3:
@@ -90,9 +138,9 @@ def check_collumns():
     global game_still_on
     # Zmienna col1 przypisana wartości z tablicy o indeksie 0 a ta pozycja tablicy jest taka sama jak pozycja 3 i 6
     # to mamy zwycięzce w pierwszej kolumnie. Aby uniknąć wygranej z powodu placeholdera - musi być też różna od -
-    col1 = board[0] == board[3] == board [6] != '-'
-    col2 = board[1] == board[4] == board [7] != '-'
-    col3 = board[2] == board[5] == board [8] != '-'
+    col1 = board[0] == board[3] == board [6] != ' '
+    col2 = board[1] == board[4] == board [7] != ' '
+    col3 = board[2] == board[5] == board [8] != ' '
 
     # Jeśli w którymś rzędzie jest wygrany to ustaw globalną zmienną na False tym samym zakończ grę
     if col1 or col2 or col3:
@@ -113,9 +161,9 @@ def check_diagonal():
     # Ustawienie zmiennej globalnej game_still_on
     global game_still_on
     # Zmienna diag1 przypisana wartości z tablicy o indeksie 0 a ta pozycja tablicy jest taka sama jak pozycja 4 i 8
-    # to mamy zwycięzce po pierwszej przekątnej. Aby uniknąć wygranej z powodu placeholdera '-' musi być też różna od'-'
-    diag1 = board[0] == board[4] == board [8] != '-'
-    diag2 = board[2] == board[4] == board [6] != '-'
+    # to mamy zwycięzce po pierwszej przekątnej. Aby uniknąć wygranej z powodu placeholdera ' ' musi być też różna od' '
+    diag1 = board[0] == board[4] == board [8] != ' '
+    diag2 = board[2] == board[4] == board [6] != ' '
 
     # Jeśli w którymś rzędzie jest wygrany to ustaw globalną zmienną na False tym samym zakończ grę
     if diag1 or diag2:
@@ -139,6 +187,7 @@ def check_winner():
     collumn_win = check_collumns()
     diagonal_win = check_diagonal()
 
+    # Funkcja if sprawdzająca czy wygrany jest po wierszach, kolumnach bądź przekątnych, ewentualnie nie ma wygranego
     if row_win:
         winner = row_win
     elif collumn_win:
@@ -154,8 +203,8 @@ def check_winner():
 def check_tie():
     # Ustawienie zmiennej globalnej game_still_on
     global game_still_on
-    # Jeśli w tablicy (na planszy gry) nie ma już żadnego '-' oznacza to, że plansza zostałą wypełniona i jest remis
-    if '-' not in board:
+    # Jeśli w tablicy (na planszy gry) nie ma już żadnego ' ' oznacza to, że plansza zostałą wypełniona i jest remis
+    if ' ' not in board:
         game_still_on = False
 
     return
@@ -184,7 +233,7 @@ def check_game_over():
 
 # Główna funkcja gry, to ona jest odpowiedzialna za całą rozgrywkę
 def game_on():
-
+    global x_wins, o_wins, ties
     # Wyświetlanie zainicjowanej tablicy z grą
     display_board()
 
@@ -201,16 +250,33 @@ def game_on():
         # Jeśli nie ma ramisu bądźwygranych zmiana gracza z X na O, z O na X.
         change_player()
 
+
     # Jeśli 'wyskoczymy' z pętli powyżej w punkcie check_if_game_over przejdziemy do polecenia if
-    # sprawdzająca któy gracz wygrał lub czy jest remis
+    # sprawdzająca któy gracz wygrał lub czy jest remis. Następnie wywoływana jest funkcja z pytaniem o potwórkę gry
     if winner == 'X' or winner == 'O':
         print(f'\nPlayer {winner} has won!')
+        # Zliczanie ilości wygranych
+        if winner == 'X':
+            x_wins += 1
+            print(f'X won {x_wins} times | O won {o_wins} times  |  Ties {ties}')
+        elif winner == 'O':
+            o_wins += 1
+            print(f'X won {x_wins} times | O won {o_wins} times  |  Ties {ties}')
+        next_game()
     elif winner == None:
         print('\nNo one won. Tie!')
+        ties += 1
+        print(f'X won {x_wins} times | O won {o_wins} times  |  Ties {ties}')
+        next_game()
 
     return
 
 print('Tic Tac Toe game! 2k20\n')
-game_on()
-# Input który zatrzymuje program przed zamknięciem się po wykonaniu gry
-input("Press enter to close program")
+# Wybieramy czy chcemy grać X czy O, przy kontynuowaniu rozgrywki zacznie drugi gracz jako pierwszy
+choose_player()
+# Pętla która odpala grę, w przypadku wyrażenia chęci rezygnacji z dalszej gry przy funkcji next_game pętla kończy się
+while play_again:
+    game_on()
+
+print('*********************************************')
+input('Thank You for playing. Press any key to exit.\n\nKamil Seternus 2020')
